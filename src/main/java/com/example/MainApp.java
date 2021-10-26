a00baf2fbbe59b757b5a69fa539fc9ccefe5a4a1
Sofia Sinani <sofsin1208@gmail.com>
2021-10-26 11:09:27 +0200

    Initial commit

---
+++ src/main/java/com/example/MainApp.java
@@ -0,0 +1,15 @@
+package com.example;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class MainApp extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
